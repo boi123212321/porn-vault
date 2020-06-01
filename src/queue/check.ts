8cb88741d7a557f3a468913f301732ce952c14cc
nevusmoonwriteup <65429521+nevusmoonwriteup@users.noreply.github.com>
2020-05-31 17:10:33 -0700

    Update src/queue/check.ts
    Co-authored-by: Christopher Caroni <christophercaroni97@gmail.com>

--- src/queue/check.ts
+++ src/queue/check.ts
@@ -55,9 +55,7 @@ export async function checkVideoFolders() {
       },
     });
     logger.log(`found ${existingScenesMap.size} missing files`);
-    existingScenesMap.forEach(async (value, key) => {
-      const _id = value;
-      const path = key;
+    existingScenesMap.forEach(async (_id, path) => {
       await missingSceneCollection.upsert(_id, new MissingScene(_id, path));
     });
     loader.succeed(`${folder} done (${numFiles} videos)`);

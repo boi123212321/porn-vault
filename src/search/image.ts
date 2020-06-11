0021c13b0b12e31394dca78d7441c50bdca9907d
ArkerJames <arkerjames@gmail.com>
2020-06-11 17:07:24 +0100

    Remove hardcoded page size from Image query

--- src/search/image.ts
+++ src/search/image.ts
@@ -16,8 +16,6 @@ import {
   filterStudios,
 } from "./common";
 
-const PAGE_SIZE = 24;
-
 export let index!: Gianna.Index<IImageSearchDoc>;
 
 const FIELDS = [
@@ -202,10 +200,13 @@ export async function searchImages(query: string, shuffleSeed = "default") {
     }
   }
 
+  // Cast to Number to prevent error in query syntax
+  const numTake = Number(options.take);
+  
   return index.search({
     query: options.query,
-    skip: options.skip || options.page * 24,
-    take: options.take || options.take || PAGE_SIZE,
+    skip: options.skip || options.page * numTake,
+    take: options.take || options.take || numTake,
     sort,
     filter,
   });
